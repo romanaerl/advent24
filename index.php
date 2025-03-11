288adb110fcdad0f42ac3647579aef7639920f30
Roman Simonov <romanaerl@gmail.com>
2025-03-11 20:52:20 +0000

    fix7

--- index.php
+++ index.php
@@ -7,19 +7,6 @@ include "altLeaderboard.php";
 
 
 
-echo "<h1>AdventOfCode'24 Custom Leaderboard</h1><br/>";
-
-echo <<<EOF
-<div width = 60%>This is a version of a leaderboard which counts only a time
-between receiving first and second star (only compares time
-spend on a task number two in any certain day). <br/>
-The leaderboard therefore is not connected to the time 
-participants started their first task, which makes it possible 
-to get places even if you can not start at midnight EST. </br></br>
-</div>
-EOF;
-
-
 $customFilename = "data/downloadedAdventJson{YEAR}.json";
 $Def = new def();
 $Def->setCustomFilename($customFilename);
@@ -32,6 +19,23 @@ $Def->reDownload();
 $year_str = "<a href = '?year={YEAR}'>{BLD}{YEAR}{/BLD}</a> ";
 
 $curYear = $Def->getYear();
+
+
+echo "<h1>AdventOfCode'" . $curYear . " Custom Leaderboard</h1><br/>";
+
+echo <<<EOF
+<div width = 60%>This is a version of a leaderboard which counts only a time
+between receiving first and second star (only compares time
+spend on a task number two in any certain day). <br/>
+The leaderboard therefore is not connected to the time 
+participants started their first task, which makes it possible 
+to get places even if you can not start at midnight EST. </br></br>
+</div>
+EOF;
+
+
+
+
 $years_str = "";
 foreach ($Def->getValidYears() as $year) {
     $year_one = $year_str;
