8097c4acc69f6c898d366ad0f580ea1231d6f1ae
Roman Simonov <romanaerl@gmail.com>
2025-03-11 23:30:37 +0000

    fix13

--- index.php
+++ index.php
@@ -40,10 +40,10 @@ $years_str = "";
 foreach ($Def->getValidYears() as $year) {
     $year_one = $year_str;
     if ($year == $Def->getYear()) {
-        $year_one = " $year ";
+        $year_one = str_replace("{YEAR}", "*$year*", $year_one);
     } else if ($year == $Def->endYear) {
         $year_one = str_replace("?year={YEAR}", '', $year_one);
-        $year_one = str_replace("{YEAR}", trim((string)$year), $year_one);
+        $year_one = str_replace("{YEAR}", $year, $year_one);
     } else {
         $year_one = str_replace("{YEAR}", "$year", $year_one);
     }
