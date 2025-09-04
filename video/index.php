6fd5f78c2e4ce61094b8c287c635c0b8d93f5fd1
Roman Simonov <romanaerl@gmail.com>
2025-09-04 08:58:36 +0100

    add video

---
+++ video/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script src='https://8x8.vc/vpaas-magic-cookie-d5cf33c9b39d4490b2cc34c06b4fada2/external_api.js' async></script>
+    <style>html, body, #jaas-container { height: 100%; }</style>
+    <script type="text/javascript">
+        window.onload = () => {
+            const api = new JitsiMeetExternalAPI("8x8.vc", {
+                roomName: "vpaas-magic-cookie-d5cf33c9b39d4490b2cc34c06b4fada2/SampleAppSolidIndictmentsLeaveTruly",
+                parentNode: document.querySelector('#jaas-container'),
+                // Make sure to include a JWT if you intend to record,
+                // make outbound calls or use any other premium features!
+                // jwt: "eyJraWQiOiJ2cGFhcy1tYWdpYy1jb29raWUtZDVjZjMzYzliMzlkNDQ5MGIyY2MzNGMwNmI0ZmFkYTIvZTA4ZWZjLVNBTVBMRV9BUFAiLCJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJqaXRzaSIsImlzcyI6ImNoYXQiLCJpYXQiOjE3NTY5NzIzNzgsImV4cCI6MTc1Njk3OTU3OCwibmJmIjoxNzU2OTcyMzczLCJzdWIiOiJ2cGFhcy1tYWdpYy1jb29raWUtZDVjZjMzYzliMzlkNDQ5MGIyY2MzNGMwNmI0ZmFkYTIiLCJjb250ZXh0Ijp7ImZlYXR1cmVzIjp7ImxpdmVzdHJlYW1pbmciOmZhbHNlLCJmaWxlLXVwbG9hZCI6ZmFsc2UsIm91dGJvdW5kLWNhbGwiOmZhbHNlLCJzaXAtb3V0Ym91bmQtY2FsbCI6ZmFsc2UsInRyYW5zY3JpcHRpb24iOmZhbHNlLCJsaXN0LXZpc2l0b3JzIjpmYWxzZSwicmVjb3JkaW5nIjpmYWxzZSwiZmxpcCI6ZmFsc2V9LCJ1c2VyIjp7ImhpZGRlbi1mcm9tLXJlY29yZGVyIjpmYWxzZSwibW9kZXJhdG9yIjp0cnVlLCJuYW1lIjoiVGVzdCBVc2VyIiwiaWQiOiJnb29nbGUtb2F1dGgyfDExNjIzODE2MTgzMTg0MjY3MzAyNSIsImF2YXRhciI6IiIsImVtYWlsIjoidGVzdC51c2VyQGNvbXBhbnkuY29tIn19LCJyb29tIjoiKiJ9.Xou5a8dvPmK5_gidgZvPgX4aU2rB2ju5tRltpIln-UMqCvOuczkKPZ2DAA4AXkar6iBZiKnUdYau-tAXCNLZFgpQSpJJ8wtR1NwIrNb8whR3IqLWd4yKx3ivGs4vRW54pX7QSVe8hXufTZh3fpbL0Zz4uy77Ey_ayBkMIcHxn9aIIYxR--8bGkA_oofp3Bc-0EyQp2SPGXnnziDVkY0v_OzZZ9XismXT89M5D48ZK48viG8vzPxniinC68QVMFNpztHkqzBBEo-7cqYOM0POxo9uFttuQLO8EibuPv5gRHsVmAH7xQD-D6sza0Fq_FHZ4T7w6MFpJE4pu-IE40nBfw"
+            });
+        }
+    </script>
+</head>
+<body><div id="jaas-container" /></body>
+</html>
+
+
+
+
